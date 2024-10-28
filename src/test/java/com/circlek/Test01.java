a1b7183842e60b40565cadbb9ed7cc697fa7b24f
PrzemyslawPrzemo <147880746+PrzemyslawPrzemo@users.noreply.github.com>
2024-10-28 15:28:49 +0100

    Minor change regarding PR

--- src/test/java/com/circlek/Test01.java
+++ src/test/java/com/circlek/Test01.java
@@ -8,36 +8,36 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class Test01 {
+ public class Test01 {
 
-    //CODE REVIEW_
-    @Test
-    public void assertTextOnPage() {
-        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
+     //CODE REVIEW_
+     @Test
+     public void assertTextOnPage() {
+         System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+         driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
 
-        //assert Remember Me Button
-        WebElement rememeberMeLabel = driver.findElement(By.id("remember_me_label"));
-        assertEquals("Remember Me", rememeberMeLabel.getText());
+         //assert Remember Me Button
+         WebElement rememeberMeLabel = driver.findElement(By.id("remember_me_label"));
+         assertEquals("Remember Me", rememeberMeLabel.getText());
 
-        //assert Sign In text
-        WebElement signInButton = driver.findElement(By.className("login"));
-        assertEquals("Sign In", signInButton.getText());
-    }
+         //assert Sign In text
+         WebElement signInButton = driver.findElement(By.className("login"));
+         assertEquals("Sign In", signInButton.getText());
+     }
 
-    @Test
-    public void checkRememberMeCheckBox() {
-        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
+     @Test
+     public void checkRememberMeCheckBox() {
+         System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+         driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
 
-        //click Remember Me checkbox
-        WebElement rememberMeCheckBox = driver.findElement(By.xpath("//*[contains(text(), \"Remember Me\")]/../input"));
-        rememberMeCheckBox.click();
+         //click Remember Me checkbox
+         WebElement rememberMeCheckBox = driver.findElement(By.xpath("//*[contains(text(), \"Remember Me\")]/../input"));
+         rememberMeCheckBox.click();
 
-        //check if clicked
-        assertEquals(rememberMeCheckBox.isSelected(), true);
+         //check if clicked
+         assertEquals(rememberMeCheckBox.isSelected(), true);
 
-    }
+     }
 }
